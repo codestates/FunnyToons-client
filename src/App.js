085c50b8f3d6aed398d3177616d4481996d623ee
ka0824 <rkgud0824@gmail.com>
2021-06-16 15:51:44 +0900

    feat4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import "./App.css";
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect, Link  } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn';
+import MyPage from './pages/MyPage';
+import Modify from './pages/Modify';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return <div className="App">Hello</div>;
+export default class App extends React.Component {
+  state = {
+    isLogin: true,
+    userInfo: null,
+  }
+
+  render() {
+    return (
+      <div>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/signin" component={SignIn} />
+              <Route exact path="/mypage" component={MyPage} />
+              <Route exact path="/mypage/modify" component={Modify} />
+              <Route exact path="/404" component={NotFound} />
+              <Redirect from ="*" to="/404" />
+            </Switch>
+          </BrowserRouter>
+      </div>
+    );
+  }
+  
 }
-
-export default App;
